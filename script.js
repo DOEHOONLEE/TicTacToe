9d29e178d9f0574bf71143f1c19e62ecfa087cdc
DOEHOONLEE <genesisparadox@gmail.com>
2020-07-01 17:05:06 +0900

    Game build done
    - Seems to be working mostly (with no bugs)

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
 const tdNum = document.querySelectorAll("td");
 const resetGameButton = document.getElementById("reset_button");
+const winCombination = [
+    "012","345","678","036","147","258","048","246"
+]
 let turn = true;
 
 for (let i=0; i < tdNum.length; i++) {
     tdNum[i].addEventListener("click", function(e) {
         if (turn) {
-            e.target.innerHTML = "O"
+            // e.target.innerHTML = "O"
+            placeUserMove(e, "O");
             turn = false;
         }
         else {
-            this.innerHTML = "X"
+            // this.innerHTML = "X"
+            placeUserMove(e, "X");
             turn = true;
         }
     })
 }
 
-resetGameButton.addEventListener("click", function() {
-    tdNum.forEach(c => c.innerHTML = "");
-})
+
+function placeUserMove(e, turn) {
+    if (e.target.innerHTML) {
+        alert("You cannot play there. Please pick another spot")
+    }
+    else {
+        e.target.innerHTML = turn;
+        winCheck(winCombination)
+    }
+}
+
+function winCheck(winCombination) {
+    
+    for (let i=0; i < winCombination.length; i++) {
+        let coord1 = document.getElementById(+(winCombination[i][0]));
+        let coord2 = document.getElementById(+(winCombination[i][1]));
+        let coord3 = document.getElementById(+(winCombination[i][2]));
+        if (coord1.innerText.length && coord2.innerText.length && coord3.innerText.length) {
+            if (coord1.innerText == coord2.innerText && coord2.innerText == coord3.innerText) {
+                alert("The winner is " + coord1.innerText);
+                reset();
+            }
+        }
+    }
+}
+
+function reset() {
+        tdNum.forEach(c => c.innerHTML = "");
+}
