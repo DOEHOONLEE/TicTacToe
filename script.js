f8493f9b7e97a4960d7ab448213ae50273b6e52c
DOEHOONLEE <genesisparadox@gmail.com>
2021-08-22 16:55:54 +0900

    game.js built with class

--- script.js
+++ script.js
@@ -1,68 +1,18 @@
+import Game from './game.js';
+
 const tdNum = document.querySelectorAll("td");
-const resetGameButton = document.getElementById("reset_button");
-const winCombination = [
-    "012","345","678","036","147","258","048","246"
-]
-let turn = true;
-let winner = "draw";
 
-for (let i=0; i < tdNum.length; i++) {
-    tdNum[i].addEventListener("click", function(e) {
-        
-        playSound();
-        
-        if (turn) {
-            // e.target.innerHTML = "O"
-            placeUserMove(e, "O");
-        }
-        else {
-            // this.innerHTML = "X"
-            placeUserMove(e, "X");
-        }
-    })
-}
+const hi = new Game(tdNum);
 
-
-function placeUserMove(e, player) {
-    if (e.target.innerHTML) {
-        alert("You cannot play there. Please pick another spot");
+document.querySelector('table').onclick = (e) => {
+    if (hi.isPlayable(e.target)) {
+        hi.placeStone(e.target);
     }
     else {
-        e.target.innerHTML = player;
-	turn = !turn;
-        winCheck(winCombination);
+        alert("Not playable. Choose some other cell");
     }
 }
 
-function winCheck(winCombination) {
-    
-    for (let i=0; i < winCombination.length; i++) {
-        let coord1 = document.getElementById(+(winCombination[i][0]));
-        let coord2 = document.getElementById(+(winCombination[i][1]));
-        let coord3 = document.getElementById(+(winCombination[i][2]));
-        if (coord1.innerText.length && coord2.innerText.length && coord3.innerText.length) {
-            if (coord1.innerText == coord2.innerText && coord2.innerText == coord3.innerText) {
-                winner = coord1.innerText;
-                alert("The winner is " + coord1.innerText);
-                reset();
-            }
-        }
-    }
-    
-    const filled = [...tdNum].filter(c => c.innerHTML.length < 1);
-    if (filled.length === 0 && winner === "draw") {
-        alert("DRAW!");
-        reset();
-        console.log(filled)
-    }
-    console.log(filled)
-}
-
-function reset() {
-        tdNum.forEach(c => c.innerHTML = "");
-}
-
-function playSound() {
-    const playSound = document.getElementById("click");
-    playSound.play();
+document.querySelector('button').onclick = () => {
+    hi.reset();
 }
